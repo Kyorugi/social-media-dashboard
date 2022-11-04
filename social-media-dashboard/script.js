e2a4dd2fbfc980278abdfcc131642404a79b3389
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2022-11-04 23:56:11 +0100

    adding modifies in HTML and CSS ande adding java script

--- social-media-dashboard/script.js
+++ social-media-dashboard/script.js
@@ -1,8 +1,12 @@
 console.log("Hello there!");
 
-// Step 1: Grab toggle button with query selector
+/* Step 1: Grab toggle button with query selector*/
 
-// Step 2: Add event listener to that button from Step 1 and listen for click
+const checkbox = document.querySelector("#checkbox");
+/* Step 2: Add event listener to that button from Step 1 and listen for click */
+console.log(checkbox);
+checkbox.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+});
 
-// Step 3: Inside of event listener add and remove (or simply toggle) "dark-theme" CSS class
-
+/* Step 3: Inside of event listener add and remove (or simply toggle) "dark-theme" CSS class */
